0236339573f3f5a52ca40fb9a7c58212ffd06447
pakwan8 <21laforgth@gmail.com>
2024-05-10 10:11:20 -0600

    add more visuals and typing

--- readall.py
+++ readall.py
@@ -1,16 +1,26 @@
+from simplegmail.message import Message
 from simplegmail import Gmail
-from time import sleep
+from time import perf_counter
+from os import remove
 
 if __name__ == '__main__':
-    gmail = Gmail()
+    gmail: Gmail = Gmail()
+
     print("getting messages...")
-    messages = gmail.get_unread_inbox()
-    total = len(messages)
+    messages: list[Message] = gmail.get_unread_inbox()
+    total: int = len(messages)
+
     print(f"total of \033[93m{total}\033[0m unread messages")
-    if input("continue? [y/n]: ").lower() == "y":
-        print("\033[33mmarking messages...\033[0m")
+    if input("continue? [y/n]: ").lower() in ["y", "yes"]:
+        i: int
+        message: Message
+        start: float = perf_counter()
         for i, message in enumerate(messages):
             print(f"marking \033[42m{message}\033[0m as read \033[90m({i + 1}/{total})\033[0m")
             message.mark_as_read()
+        end: float = perf_counter()
 
-    print("\033[96mdone!\033[0m")
+        print(f"\033[96mfinished in \033[1m{end - start:.2f}s\033[0m\033[0m")
+        remove("gmail_token.json")
+    else:
+        print("\033[96mabort\033[0m")
