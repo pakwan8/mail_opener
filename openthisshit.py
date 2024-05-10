2ed7325d69e65631ffa6169af1d3a42e825316b5
pakwan8 <21laforgth@gmail.com>
2024-05-09 23:58:02 -0600

    added progress

--- openthisshit.py
+++ openthisshit.py
@@ -1,13 +1,16 @@
 from simplegmail import Gmail
+from time import sleep
 
 if __name__ == '__main__':
     gmail = Gmail()
     print("getting messages...")
     messages = gmail.get_unread_inbox()
-    print(f"total of \033[93m{len(messages)}\033[0m unread messages")
+    total = len(messages)
+    print(f"total of \033[93m{total}\033[0m unread messages")
     if input("continue? [y/n]: ").lower() == "y":
-        for message in messages:
-            print(f"marking \033[42m{message}\033[0m as read")
+        print("\033[33mmarking messages...\033[0m")
+        for i, message in enumerate(messages):
+            print(f"marking \033[42m{message}\033[0m as read \033[90m({i + 1}/{total})\033[0m")
             message.mark_as_read()
 
-    print("\033[92mdone\033[0m")
+    print("\033[96mdone!\033[0m")
